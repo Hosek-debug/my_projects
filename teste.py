786cd4e24b8ec9aabc32e51f5e0c93b212567ad2
Hosek-debug <hosekawork@gmail.com>
2026-06-03 21:05:07 -0400

    Fisrt test

---
+++ teste.py
@@ -0,0 +1,13 @@
+print("================My first test using GitBash in my house===================")
+print("Hi, my surname is hosek, i like to writte in english and i study ADS from Senac college")
+print("Now, i will do my first test using Gitbash in my house!")
+
+print("First question, what's the teacher from Programation Logic always says when he start to speak about Python?")
+print("Answer 1 - Pyteiro!, Answer 2 - Javeiro!")
+
+answer = str(input("Writte your answer"))
+
+if answer == ("Pyteiro!"):
+    print("That's right!!")
+else:
+    print("almost that, but, Javeiro!!")
